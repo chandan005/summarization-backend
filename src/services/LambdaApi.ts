733f443057ed3e83e5152e738f554f1972326d67
chandansingh005 <chandan.singh@brauz.com>
2024-03-11 12:23:30 +0545

    DynamoDB Put and Get All

--- src/services/LambdaApi.ts
+++ src/services/LambdaApi.ts
@@ -1,9 +1,10 @@
 import Joi from 'joi';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateSummarizationDto } from '../dto/CreateSummarizationDto';
+import { FindSummarizationQueryDto } from '../dto/FindSummarizationQueryDto';
 import { SummarizationResponseDto } from '../dto/SummarizationResponseDto';
 import { ISummarize } from '../entities/ISummarize';
-import { getItem, putItem } from '../repository/Summarize.Repository';
+import { getItem, getItems, putItem } from '../repository/Summarize.Repository';
 
 export async function createSummarization(payload: any): Promise<ISummarize> {
   const schema = Joi.object<CreateSummarizationDto>({
@@ -33,5 +34,20 @@ export async function createSummarization(payload: any): Promise<ISummarize> {
 export async function findSummarizations(
   query: any,
 ): Promise<SummarizationResponseDto | undefined> {
-  return undefined;
+  const schema = Joi.object<FindSummarizationQueryDto>({
+    count: Joi.number().required(),
+    nextPageKey: Joi.string().optional(),
+  });
+
+  const { error: validationError } = schema.validate(query);
+  if (validationError) {
+    throw new Error(validationError.message);
+  }
+
+  try {
+    const { count = 25, nextPageKey } = query as FindSummarizationQueryDto;
+    return getItems(count, nextPageKey);
+  } catch (err: any) {
+    throw new Error(`${err.message}`);
+  }
 }
